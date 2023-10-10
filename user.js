d591887f9f6bfc61d93bcf269e4f40552e2db991
Icey-Glitch <icey-glitch@riseup.net>
2023-10-10 16:20:25 -0700

    font rendering

--- user.js
+++ user.js
@@ -1,3 +1,9 @@
+// font rendering
+// Fix Windows font rendering
+user_pref("gfx.font_rendering.directwrite.bold_simulation", 2);
+user_pref("gfx.font_rendering.cleartype_params.enhanced_contrast", 25);
+user_pref("gfx.font_rendering.cleartype_params.force_gdi_classic_for_families", "");
+
 // ** Theme Default Options ****************************************************
 // userchrome.css usercontent.css activate
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
